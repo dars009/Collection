4e569048da6b8d14c57c6099aa3eb3d121ed3f14
Darshit R <darshit.rabadiya@hotmail.com>
2021-06-23 19:02:01 +0530

    List object added

--- CollectionDemo/src/com/test/ListDemo.java
+++ CollectionDemo/src/com/test/ListDemo.java
@@ -1,7 +1,12 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListDemo {
 	public static void main(String[] args) {
+		
+		List<String> ll= new ArrayList<String>();
 
 	}
 }
